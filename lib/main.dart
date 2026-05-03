2db0c5d99b30f89ac56dc1122e79d454d3e1b579
Teja Varshith <learnatyour@gmail.com>
2026-05-04 01:23:20 +0530

    chore: add dependency -firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:resume_revive/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async{
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
+
   runApp(const MyApp());
 }
 
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:auth/main.dart';
+import 'package:resume_revive/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
